993485b679649c23cfcb8fd2dde750b5f1e1aceb
ghanigh <rhani.94@proton.me>
2025-04-30 01:21:30 +0200

    hello world

---
+++ hello-word/target/debug/deps/hello_word-143937b3066d3ea7.d
@@ -0,0 +1,5 @@
+/home/laparadenoire/Documents/rust_learning/hello-word/target/debug/deps/hello_word-143937b3066d3ea7: src/main.rs
+
+/home/laparadenoire/Documents/rust_learning/hello-word/target/debug/deps/hello_word-143937b3066d3ea7.d: src/main.rs
+
+src/main.rs:
---
+++ hello-word/target/debug/hello-word.d
@@ -0,0 +1,1 @@
+/home/laparadenoire/Documents/rust_learning/hello-word/target/debug/hello-word: /home/laparadenoire/Documents/rust_learning/hello-word/src/main.rs
